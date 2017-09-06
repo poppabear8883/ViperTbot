70edd1721a382592a53f5e0dda191e84c4e848dd
poppabear <servnx@gmail.com>
2017-09-06 16:01:59 -0400

    Added Templates

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copy('node_modules/font-awesome/fonts', 'public/fonts')
+    .styles([
+            'public/css/vendor/smartadmin-production-plugins.css',
+            'public/css/vendor/smartadmin-production.css',
+            'public/css/vendor/smartadmin-skins.min.css'
+        ],
+        'public/css/smartadmin.css');
